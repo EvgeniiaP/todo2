3bfa8873c0f7d73639afc87fa91bee08a473e591
EvgeniiaP <chitamarg@gmail.com>
2020-05-05 15:35:53 +0300

    add deploy CI

---
+++ deploy.js
@@ -0,0 +1,27 @@
+const Client = require('ftp');
+const fs = require('fs');
+const folderDist = './dist/';
+const upLoadFolder = 'public_html';
+
+var c = new Client();
+
+c.on('ready', function() {
+  c.rmdir(upLoadFolder, true, () => {
+    c.mkdir(upLoadFolder, false, () => {
+      fs.readdirSync(folderDist).forEach(fileName =>{
+        c.put(folderDist + fileName, upLoadFolder + '/' + fileName, function(err){
+          if (err) {throw err;}
+          c.end();
+        });
+      });
+    });
+  });
+});
+
+let connectOption = {
+  host: 'probapera202-ru.1gb.ru',
+  user: 'w_probapera202-ru_cc940b2f	',
+  password: 'c88ca29z5a'
+}
+
+c.connect(connectOption);
